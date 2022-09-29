ca8243d4207753a6cd1df6f62eb246ad0444068a
George Dialektakis <geo4diale@gmail.com>
2022-09-29 17:17:53 +0300

    Data testing

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,56 +1,100 @@
 #!/usr/bin/env python
 """
-An example of a step using MLflow and Weights & Biases]: Performs basic cleaning on the data and save the results in Weights & Biases
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
 """
 import argparse
 import logging
 import wandb
-
+import pandas as pd
+import pandas_profiling
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
 def go(args):
-
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
 
     ######################
     # YOUR CODE HERE     #
     ######################
 
+    logger.info("Downloading and reading input artifact")
+    artifact_local_path = run.use_artifact("sample.csv:latest").file()
+
+    df = pd.read_csv(artifact_local_path, low_memory=False)
+
+    profile = pandas_profiling.ProfileReport(df)
+    profile.to_widgets()
+
+    # Drop outliers
+    min_price = args.min_price
+    max_price = args.max_price
+
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    df.to_csv("clean_sample.csv", index=False)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description="This steps cleans the data")
-
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
     parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--input_artifact",
+        type=str,
+        help="The input artifact",
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_artifact",
+        type=str,
+        help="The output artifact",
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_type",
+        type=str,
+        help="The output type",
         required=True
     )
 
+    parser.add_argument(
+        "--output_description",
+        type=str,
+        help="The output description",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price",
+        type=float,
+        help="Minimum accepted price",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=float,
+        help="Maximum accepted price",
+        required=True
+    )
 
     args = parser.parse_args()
 
